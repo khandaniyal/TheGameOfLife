1700102f80bad9d66ed391a39499f51fef8a7a56
khandaniyal <daniyal.khan@iesjoandaustria.org>
2021-10-26 23:17:51 +0200

    moved countNeighbour method to cells

--- Board.java
+++ Board.java
@@ -5,16 +5,19 @@ public class Board extends JFrame {
 
     private int BOARD_SIZE = 25,
                       ANIMATION_DELAY = 100;//milliseconds;
-    private final Cell[][] cells;
+    private final Cells[][] cells;
+    private final Cells cell;
     private JPanel universPanel;
     private JPanel panelButton;
+    private int count = 0;
 
     public Board(int BOARD_SIZE) {
         this.BOARD_SIZE = BOARD_SIZE;
         //initialize constants
         universPanel = new JPanel();
         panelButton = new JPanel();
-        cells = new Cell[BOARD_SIZE][BOARD_SIZE];
+        cell = new Cells();
+        cells = new Cells[BOARD_SIZE][BOARD_SIZE];
         //add a grid
         universPanel.setLayout(new GridLayout(BOARD_SIZE, BOARD_SIZE));
         //initialize all cells
@@ -43,7 +46,7 @@ public class Board extends JFrame {
     private void animate(){
         for(int i = 0; i < BOARD_SIZE; i++) {
             for(int j = 0; j < BOARD_SIZE; j++) {
-                int count = numberOfNeigboursAround(i, j);
+                count = cell.countNeighbours(i, j, cells);
                 if(cells[i][j].isUniverse()){
                     if(count < 2) {
                         cells[i][j].setUniverse(false);
@@ -58,22 +61,10 @@ public class Board extends JFrame {
             }
         }
     }
-    private int numberOfNeigboursAround(int x, int y) {
-        int count = 0;
-        for(int i = x - 1; i <= x + 1; i++) {
-            for(int j = y - 1; j <= y + 1; j++) {
-                try {
-                    if(cells[i][j].isUniverse()) count++; //counts each cell
-                }catch(Exception e){ }
-            }
-        }
-        if(cells[x][y].isUniverse()) count--; //reduces the cell count until you get the count of the surrounding cells of the alive cell
-        return count;
-    }
-    public void initializeAllCells(Cell[][] cells){
+    public void initializeAllCells(Cells[][] cells){
         for(int x = 0; x < BOARD_SIZE; x++) {
             for(int y= 0; y < BOARD_SIZE; y++) {
-                Cell cell = new Cell();
+                Cells cell = new Cells();
                 universPanel.add(cell);
                 cells[x][y] = cell;
             }
