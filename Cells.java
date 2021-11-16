258b88adcde3ee75dc0cbb001183638ccb5b5564
joaquingb4 <43388641+joaquingb4@users.noreply.github.com>
2021-11-16 16:51:23 +0100

    Cells with bugs

--- Cells.java
+++ Cells.java
@@ -3,14 +3,13 @@ import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
-public class Cells extends JLabel {
-
+public class Cells extends JLabel{
     private static Color BOARD = Color.BLACK,
             UNIVERSE = Color.GREEN,
             BORDER = Color.GRAY;
     private static Dimension CELL_SIZE;
     private boolean universe = false;
-    private int neighbours = 0;
+    private int neighbours;
 
     public Cells() { //Constructor calling the following methods to create the universe
         super();
@@ -19,8 +18,9 @@ public class Cells extends JLabel {
         mouseListener();
     }
     /*Getters & Setters*/
+    public int getNeighbours(){ return neighbours; }
+    public void setNeighbours(int neighbours){ this.neighbours = neighbours; }
     public Dimension getPreferredSize() { return CELL_SIZE; } //Current size of the cells
-    public int getNeighbours() { return neighbours; }
     public boolean isUniverse() { return universe; }
     public void setUniverse(boolean isUniverse) { //checks the state of the univers and paints the univers or the board
         if (universe == isUniverse) return;
@@ -31,16 +31,16 @@ public class Cells extends JLabel {
     /* Extra methods */
     public void mouseClickedHandler(MouseEvent e) { //Method for our mouse input
         if (SwingUtilities.isLeftMouseButton(e)) //Checks if the left mouse button is clicked, in this case we want to paint
-
             setUniverse(true);
         else
-            if (SwingUtilities.isRightMouseButton(e)) //In the other hand we want to erase the painted cells
+            if (SwingUtilities.isRightMouseButton(e)) //Otherwise we want to erase the painted cells
                 setUniverse(false);
     }
     public void createUnivers() { //Create the universe with the following visual elements
         setUniverse(universe);
         setOpaque(true);
         setBorder(BorderFactory.createLineBorder(BORDER, 2));
+        setBackground(BOARD);
     }
     public void mouseListener() { //Method for the mouse input listener
         addMouseListener(new MouseInputAdapter() {
@@ -50,5 +50,17 @@ public class Cells extends JLabel {
             public void mouseEntered(MouseEvent e) { mouseClickedHandler(e); } //This event listens when the mouse is pressed, in this case we want to draw while the mouse is being pressed
         });
     }
-}
 
+    public int countNeighbours(int x, int y, Cells[][] cells){
+        neighbours = 0;
+        for(int i = x - 1; i <= x + 1; i++) {
+            for(int j = y - 1; j <= y + 1; j++) {
+                try {
+                    if(cells[i][j].isUniverse()) neighbours++; //counts each cell
+                }catch(Exception e){ }
+            }
+        }
+        if(cells[x][y].isUniverse()) neighbours--; //reduces the cell count until you get the count of the surrounding cells of the alive cell
+        return neighbours;
+    }
+}
