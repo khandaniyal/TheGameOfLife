88762da302b51cfa54407478874baa2ee66596db
khandaniyal <57764937+khandaniyal@users.noreply.github.com>
2021-11-17 10:33:16 +0100

    Delete UserContracts.java

--- UserContracts.java
+++
@@ -1,7 +0,0 @@
-
-public class UserContracts {
-    public static final String TABLE_NAME = "users";
-    public static final String TABLE_ID = "id";
-    public static final String TABLE_USERNAME = "userName";
-    public static final String TABLE_PASSWORD = "password";
-}
